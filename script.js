11ded074f78f155faa835ac98d2b6566539597eb
donny <donny.vallejo@gmail.com>
2017-04-06 18:13:48 -0700

    added new car img's and staarted animation for race :)

--- script.js
+++ script.js
@@ -15,37 +15,16 @@ function movePlane() {
     for (var direction in keys) {
         if (!keys.hasOwnProperty(direction)) continue;
         if (direction == 37) {
-            $("#plane").animate({left: "-=5"}, 0);
+            $("#carA").animate({left: "-=5"}, 0);
         }
         if (direction == 38) {
-            $("#plane").animate({top: "-=5"}, 0);
+            $("#carA").animate({top: "-=5"}, 0);
         }
         if (direction == 39) {
-            $("#plane").animate({left: "+=5"}, 0);
+            $("#carA").animate({left: "+=7"}, 0);
         }
         if (direction == 40) {
-            $("#plane").animate({top: "+=5"}, 0);
+            $("#carA").animate({top: "+=5"}, 0);
         }
     }
-}
-
-// var pane = $('#pane'),
-//     box = $('#box'),
-//     w = pane.width() - box.width(),
-//     d = {},
-//     x = 3;
-//
-// function newv(v,a,b) {
-//     var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
-//     return n < 0 ? 0 : n > w ? w : n;
-// }
-//
-// $(window).keydown(function(e) { d[e.which] = true; });
-// $(window).keyup(function(e) { d[e.which] = false; });
-//
-// setInterval(function() {
-//     box.css({
-//         left: function(i,v) { return newv(v, 37, 39); },
-//         top: function(i,v) { return newv(v, 38, 40); }
-//     });
-// }, 20);
+};
